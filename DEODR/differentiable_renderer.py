fixing some gradients

DEODR/differentiable_renderer.py
@@ -140,8 +140,8 @@ class Scene3D():
         r = np.column_stack((P3D, np.ones((P3D.shape[0],1), dtype = np.double))).dot(cameraMatrix.T)
         depths = r[:,2]
         #P2D = r[:,:2]/depths[:,None]
-        r_b = np.column_stack((P2D_b/depths[:,None], -P2D_b*r[:,:2]/(depths[:,None]**2))) 
-        P3D_b = r.dot(cameraMatrix[:,:3])
+        r_b = np.column_stack((P2D_b/depths[:,None], -np.sum(P2D_b*r[:,:2],axis=1)/(depths**2))) 
+        P3D_b = r_b.dot(cameraMatrix[:,:3])
         return P3D_b        
         
     def computeVerticesColorsWithIllumination(self):
@@ -154,12 +154,12 @@ class Scene3D():
         directional = np.maximum(0,-np.sum(self.mesh.vertexNormals * self.ligthDirectional, axis = 1))
         verticesLuminosity = directional + self.ambiantLight
         
-        verticesLuminosity_b = self.mesh.verticesColors * colors_b
+        verticesLuminosity_b = np.sum(self.mesh.verticesColors * colors_b,axis=1)
         self.mesh.verticesColors_b = colors_b * verticesLuminosity[:,None]
         self.ambiantLight_b = np.sum(verticesLuminosity_b)
         directional_b = verticesLuminosity_b 
-        self.lightDirectional_b = np.sum((directional_b*(directional>0)[:,None]) * self.mesh.vertexNormals,axis=0)
-        self.vertexNormals_b = (directional_b*(directional>0)[:,None]) * self.ligthDirectional
+        self.lightDirectional_b = -np.sum(((directional_b*(directional>0))[:,None]) * self.mesh.vertexNormals,axis=0)
+        self.vertexNormals_b = -((directional_b*(directional>0))[:,None]) * self.ligthDirectional
     
     def render2D(self,ij,colors):   
         nbColorChanels = colors.shape[1]

DEODR/examples/rgb_image_hand_fitting.py
@@ -28,6 +28,7 @@ def example_rgb_hand_fitting(dl_library='pytorch', plot_curves=True):
     
     defaultColor = np.array([0.4,0.3,0.25])
     defaultLight = {'directional':np.array([0.1,0.5,0.4]),'ambiant':np.array([0.6])} 
+    #defaultLight = {'directional':np.array([0.0,0.0,0.0]),'ambiant':np.array([0.6])} 
 
     euler_init = np.zeros((3))
     translation_init = np.zeros(3)
@@ -88,6 +89,10 @@ def example_rgb_hand_fitting(dl_library='pytorch', plot_curves=True):
         plt.show()  
 
 if __name__ == "__main__":
-    example_rgb_hand_fitting(dl_library='none',plot_curves=False)
     example_rgb_hand_fitting(dl_library='pytorch',plot_curves=False)
+    example_rgb_hand_fitting(dl_library='none',plot_curves=False)
+    
+    
+    
+    
     example_rgb_hand_fitting(dl_library='tensorflow')

DEODR/pytorch/differentiable_renderer_pytorch.py
@@ -51,7 +51,7 @@ class Scene3DPytorch(Scene3D):
         return P2D,depths     
  
     def computeVerticesColorsWithIllumination(self):
-        verticesLuminosity = torch.relu(-torch.sum(self.mesh.vertexNormals * self.ligthDirectional, dim = 1)) + self.ambiantLight
+        verticesLuminosity = torch.relu(-torch.sum(self.mesh.vertexNormals * self.ligthDirectional, dim = 1)) + self.ambiantLight      
         return self.mesh.verticesColors * verticesLuminosity[:,None]      
     
     def render2D(self,ij,colors):   

DEODR/pytorch/triangulated_mesh_pytorch.py
@@ -3,6 +3,13 @@ import numpy as np
 import torch
 from ..triangulated_mesh import *
 
+
+def print_grad(name):
+	def hook(grad):
+		print(f'grad {name} = {grad}')
+	return hook
+
+
 class TriMeshAdjacenciesPytorch(TriMeshAdjacencies):
 	def __init__(self,faces):
 		super().__init__(faces)
@@ -16,11 +23,18 @@ class TriMeshAdjacenciesPytorch(TriMeshAdjacencies):
 		tris = vertices[self.faces_torch,:]
 		n = torch.cross( tris[::,1 ] - tris[::,0], tris[::,2 ] - tris[::,0] )
 		l = ((n**2).sum(dim = 1)).sqrt()
+		vertices.register_hook(print_grad('vertices'))	
+		tris.register_hook(print_grad('tris'))	
 		return n/l[:,None]
 		
 	def computeVertexNormals(self,faceNormals):
 		n = self.Vertices_Faces_torch.mm(faceNormals)
-		l = ((n**2).sum(dim = 1)).sqrt()
+		l2= ((n**2).sum(dim = 1))
+		l =l2.sqrt()
+		n.register_hook(print_grad('l2'))
+		n.register_hook(print_grad('n'))
+		l.register_hook(print_grad('l'))
+		faceNormals.register_hook(print_grad('faceNormals'))	
 		return  n/l[:,None]
 	
 	def edgeOnSilhouette(self, vertices, faceNormals, viewpoint):

DEODR/tools.py
@@ -25,10 +25,11 @@ def normalize(x,axis=-1):
 def normalize_backward(x, xn_b, axis=-1 ):
     n2 = np.sum(x**2,axis = axis)
     n = np.sqrt(n2)
-    inv_n = np.expand_dims(1/n,axis)
-    x_b = xn_b * inv_n
-    n_b = -xn_b * x * (inv_n**2)
-    x_b -= x*n_b*inv_n
+    inv_n = 1/n
+    
+    n_b = -np.sum(xn_b * x,axis=1) * (inv_n**2)
+    n2_b = 0.5*n_b/n
+    x_b = (xn_b- x*np.expand_dims(n_b,axis))*np.expand_dims(inv_n,axis)
     return x_b
 
 def cross_backward(u,v,c_b):