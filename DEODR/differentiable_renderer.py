fixing problems with gradient for depth image fitting

DEODR/differentiable_renderer.py
@@ -136,12 +136,15 @@ class Scene3D():
         P2D,depths,J_P2D = self.camera_project(CameraMatrix, vertices, get_jacobians=True) 
         return J_P2D     
     
-    def cameraProject_backward(self,cameraMatrix, P3D, P2D_b) :
+    def cameraProject_backward(self,cameraMatrix, P3D, P2D_b,depths_b=None) :
         r = np.column_stack((P3D, np.ones((P3D.shape[0],1), dtype = np.double))).dot(cameraMatrix.T)
         depths = r[:,2]
         #P2D = r[:,:2]/depths[:,None]
         r_b = np.column_stack((P2D_b/depths[:,None], -np.sum(P2D_b*r[:,:2],axis=1)/(depths**2))) 
+        if depths_b is not None:
+            r_b[:,2]+=depths_b
         P3D_b = r_b.dot(cameraMatrix[:,:3])
+            
         return P3D_b        
         
     def computeVerticesColorsWithIllumination(self):
@@ -235,10 +238,11 @@ class Scene3D():
         Abuffer = self.render2D(ij,colors)
         return Abuffer
     
-    def renderDepth_backward(self,CameraMatrix, Depth_b):
+    def renderDepth_backward(self,CameraMatrix,depth_scale,Depth_b):
         cameraCenter3D = -np.linalg.solve(CameraMatrix[:3,:3], CameraMatrix[:,3]) 
-        ij_b, colors_b = self.render2D_backward(Depth_b)        
-        self.mesh.vertices_b = self.cameraProject_backward(CameraMatrix, self.mesh.vertices, ij_b)
+        ij_b, colors_b = self.render2D_backward(Depth_b)   
+        depths_b= np.squeeze(colors_b*depth_scale,axis=1)
+        self.mesh.vertices_b = self.cameraProject_backward(CameraMatrix, self.mesh.vertices, ij_b,depths_b)
           
     
 

DEODR/examples/depth_image_hand_fitting.py
@@ -11,7 +11,7 @@ import os
 import json
 
 
-def example_depth_image_hand_fitting(dl_library = 'pytorch', plot_curves = True,save_images = True):
+def example_depth_image_hand_fitting(dl_library = 'pytorch', plot_curves = True,save_images = True,display = True):
     
     if dl_library=='pytorch':
         from DEODR.pytorch import MeshDepthFitter
@@ -54,10 +54,12 @@ def example_depth_image_hand_fitting(dl_library = 'pytorch', plot_curves = True,
         Energy,syntheticDepth,diffImage = handFitter.step()
         Energies.append(Energy)
         durations.append(time.time() - start)
-        combinedIMage = np.column_stack((depth_image, syntheticDepth, 3*diffImage))
-        cv2.imshow('animation', cv2.resize(combinedIMage, None, fx = 2, fy = 2)) 
-        if save_images:
-            imsave(os.path.join(iterfolder, f'depth_hand_iter_{iter}.png'), combinedIMage)
+        if save_images or display:
+            combinedIMage = np.column_stack((depth_image, syntheticDepth, 3*diffImage))
+            if display:
+                cv2.imshow('animation', cv2.resize(combinedIMage, None, fx = 2, fy = 2)) 
+            if save_images:
+                imsave(os.path.join(iterfolder, f'depth_hand_iter_{iter}.png'), combinedIMage)
         key = cv2.waitKey(1) 
         
     with open(os.path.join(iterfolder, 'depth_image_fitting_result_%s.json'%str(datetime.datetime.now()).replace(':','_')), 'w') as f:
@@ -79,12 +81,10 @@ def example_depth_image_hand_fitting(dl_library = 'pytorch', plot_curves = True,
         plt.show()    
  
 if __name__ == "__main__":
-    example_depth_image_hand_fitting(dl_library='none', plot_curves=False, save_images = False)
-    example_depth_image_hand_fitting(dl_library='pytorch', plot_curves=True, save_images = False)
-    
-    
-    
-    example_depth_image_hand_fitting(dl_library='tensorflow', plot_curves=False, save_images = False)
+    display=False
+    example_depth_image_hand_fitting(dl_library='none', plot_curves=False, save_images = False, display= display)
+    example_depth_image_hand_fitting(dl_library='pytorch', plot_curves=False, save_images = False,display= display)
+    example_depth_image_hand_fitting(dl_library='tensorflow', plot_curves=True, save_images = False,display= display)
   
 
     

DEODR/examples/rgb_image_hand_fitting.py
@@ -10,7 +10,7 @@ import glob
 import json
 import os
 
-def example_rgb_hand_fitting(dl_library='pytorch', plot_curves=True, save_images=True):
+def example_rgb_hand_fitting(dl_library='pytorch', plot_curves=True, save_images=True, display = True):
     if dl_library == 'pytorch':
         from  DEODR.pytorch import MeshRGBFitterWithPose
     elif dl_library == 'tensorflow':
@@ -56,10 +56,12 @@ def example_rgb_hand_fitting(dl_library='pytorch', plot_curves=True, save_images
         Energy, Abuffer, diffImage = handFitter.step()
         Energies.append(Energy)
         durations.append( time.time() - start  )
-        combinedIMage = np.column_stack((handImage,Abuffer,np.tile(diffImage[:,:,None],(1,1,3))))
-        cv2.imshow('animation', cv2.resize(combinedIMage[:,:,::-1], None, fx=2, fy=2)) 
-        if save_images:
-            imsave(os.path.join(iterfolder,f'hand_iter_{iter}.png'), combinedIMage)
+        if display or save_images:
+            combinedIMage = np.column_stack((handImage,Abuffer,np.tile(diffImage[:,:,None],(1,1,3))))
+            if display:
+                cv2.imshow('animation', cv2.resize(combinedIMage[:,:,::-1], None, fx=2, fy=2)) 
+            if save_images:
+                imsave(os.path.join(iterfolder,f'hand_iter_{iter}.png'), combinedIMage)
         key = cv2.waitKey(1)  
 
        
@@ -88,6 +90,8 @@ def example_rgb_hand_fitting(dl_library='pytorch', plot_curves=True, save_images
         plt.show()  
 
 if __name__ == "__main__":
-    example_rgb_hand_fitting(dl_library='none', plot_curves = False)
-    example_rgb_hand_fitting(dl_library='pytorch', plot_curves = False)    
-    example_rgb_hand_fitting(dl_library='tensorflow')
+    display = False
+    save_images = False
+    example_rgb_hand_fitting(dl_library='none', plot_curves = False, display = display, save_images = save_images)
+    example_rgb_hand_fitting(dl_library='pytorch', plot_curves = False, display = display, save_images = save_images)    
+    example_rgb_hand_fitting(dl_library='tensorflow', plot_curves = True, display = display, save_images = save_images)

DEODR/mesh_fitter.py
@@ -74,14 +74,15 @@ class MeshDepthFitter():
         q_normalized = normalize(self.transformQuaternion) # that will lead to a gradient that is in the tangeant space
         vertices_transformed = qrot(q_normalized, self.vertices) + self.transformTranslation
         self.mesh.setVertices(vertices_transformed)   
-        depth_scale = 1 * self.depthScale
-        Depth = self.scene.renderDepth(self.CameraMatrix,resolution=(self.SizeW,self.SizeH),depth_scale=depth_scale)
-        Depth = np.clip(Depth,0,self.scene.maxDepth)   
+        self.DepthNotCliped = self.scene.renderDepth(self.CameraMatrix,resolution=(self.SizeW,self.SizeH),depth_scale = self.depthScale)
+        Depth = np.clip(self.DepthNotCliped,0,self.scene.maxDepth)   
         return Depth
     
     def render_backward(self, Depth_b):
         self.scene.clear_gradients()
-        self.scene.renderDepth_backward(self.CameraMatrix, Depth_b)
+        Depth_b[self.DepthNotCliped<0]=0
+        Depth_b[self.DepthNotCliped>self.scene.maxDepth]=0
+        self.scene.renderDepth_backward(self.CameraMatrix, self.depthScale, Depth_b )
         vertices_transformed_b = self.scene.mesh.vertices_b
         self.transformTranslation_b = np.sum(vertices_transformed_b,axis=0)
         q_normalized = normalize(self.transformQuaternion)