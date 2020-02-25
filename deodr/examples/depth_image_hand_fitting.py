dbd37fe68ef8731d26ded187e6ea73cbb49670c3
Martin de La Gorce <madelago@microsoft.com>
2020-02-25 13:46:28 +0000

    fix pub

--- deodr/examples/depth_image_hand_fitting.py
+++ deodr/examples/depth_image_hand_fitting.py
@@ -14,7 +14,6 @@ import json
 def run(
     dl_library="none", plot_curves=False, save_images=False, display=True, max_iter=150
 ):
-    if display:
         
     file_folder = os.path.dirname(__file__)
 
