bb52e723fd205c404ea327cf32fb7aeb9577b84d
Martin de La Gorce <martin.delagorce@gmail.com>
2020-03-02 23:10:07 +0000

    update version to 0.1.5

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ my_modules = cythonize(extensions, annotate=True, language="c++")
 libname = "deodr"
 setup(
     name=libname,
-    version="0.1.4",
+    version="0.1.5",
     author="Martin de La Gorce",
     author_email="martin.delagorce@gmail.com",
     description="A differentiable renderer with Pytorch,\
