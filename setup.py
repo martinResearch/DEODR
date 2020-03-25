9add4c99df9c78cfeb94282597b29eb6e1991932
Martin de La Gorce <martin.delagorce@gmail.com>
2020-03-25 17:45:50 +0000

    update version to 0.1.11

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ libname = "deodr"
 
 setup(
     name=libname,
-    version="0.1.10",
+    version="0.1.11",
     author="Martin de La Gorce",
     author_email="martin.delagorce@gmail.com",
     description="A differentiable renderer with Pytorch,Tensorflow and Matlab interfaces.",
