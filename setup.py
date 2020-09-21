d5c9bda1ad85367e411e6f6c254d1f1507679936
Nate Dana <natedana@denali-ndMBP2019.local>
2020-09-21 16:10:25 -0600

    include C++ file and change numpy import

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import numpy as np
 
 extensions = "deodr/differentiable_renderer_cython.pyx"
 
-my_modules = cythonize(extensions, annotate=True, language="c++")
+my_modules = cythonize(extensions, annotate=True, language="c++", include_path=[np.get_include()])
 
 libname = "deodr"
 
@@ -33,8 +33,8 @@ setup(
     license="BSD",
     packages=find_packages(),
     package_data={"deodr": ["*.pyx", "*.pxd", "data/*.*", "data/**/*.*"]},
+    data_files=[('C++', ['C++/DifferentiableRenderer.h'])],
     ext_modules=my_modules,  # additional source file(s)),
-    include_dirs=[np.get_include()],
     setup_requires=["numpy", "scipy", "cython"],
     install_requires=["numpy", "scipy"],
 )
