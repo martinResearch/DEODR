ca2fe9d807d10d3145b7de02635bef3557a64b65
Martin de La Gorce <madelago@microsoft.com>
2020-09-24 14:47:04 +0100

    updating test results

--- tests/test_render_mesh.py
+++ tests/test_render_mesh.py
@@ -14,9 +14,15 @@ def test_render_mesh_moderngl():
     example_moderngl(display=False)
 
 
-def test_render_mesh():
+def test_render_mesh(update=False):
     image = example_rgb(display=False, save_image=False, width=320, height=240)
     image_file = os.path.abspath(os.path.join(deodr.data_path, "test/duck.png"))
     image_uint8 = (image * 255).astype(np.uint8)
+    if update:
+        imageio.imwrite(image_file, image_uint8)
     image_prev = imageio.imread(image_file)
     assert np.max(np.abs(image_prev - image_uint8)) == 0
+
+
+if __name__ == "__main__":
+    test_render_mesh()
