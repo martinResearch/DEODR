b3729214772d41bc9fc9e969443377d0a8b1c030
Martin de La Gorce <madelago@microsoft.com>
2021-12-15 20:09:02 +0000

    adding check based on hashes

--- tests/test_render_mesh.py
+++ tests/test_render_mesh.py
@@ -4,7 +4,8 @@ import os
 
 import deodr
 from deodr.examples.render_mesh import example_moderngl, example_rgb
-
+from deodr.examples.triangle_soup_fitting import create_example_scene
+from deodr import differentiable_renderer_cython
 
 import imageio
 
@@ -16,7 +17,7 @@ def test_render_mesh_moderngl():
     example_moderngl(display=False)
 
 
-def test_render_mesh(update=False):
+def test_render_mesh_duck(update=False):
     image = example_rgb(display=False, save_image=False, width=320, height=240)
     image_file = os.path.abspath(os.path.join(deodr.data_path, "test/duck.png"))
     image_uint8 = (image * 255).astype(np.uint8)
@@ -31,6 +32,26 @@ def test_render_mesh(update=False):
     )
 
 
+def test_render_mesh_triangle_soup():
+
+    np.random.seed(2)
+    scene_gt = create_example_scene(clockwise=True)
+    sigma = 1
+    image = np.zeros((scene_gt.height, scene_gt.width, scene_gt.nb_colors))
+    z_buffer = np.zeros((scene_gt.height, scene_gt.width))
+    differentiable_renderer_cython.renderScene(scene_gt, sigma, image, z_buffer)
+
+    assert (
+        hashlib.sha256(image.tobytes()).hexdigest()
+        == "4de52cc3e902f92ff64324b261ddc45cd6d148ec7e670cf2942532d515af62d8"
+    )
+    assert (
+        hashlib.sha256(z_buffer.tobytes()).hexdigest()
+        == "b6f87e03c60bd820efa09d0536495b25d5852f67ecbecd2622f8bf1910d6052a"
+    )
+
+
 if __name__ == "__main__":
-    test_render_mesh()
+    test_render_mesh_duck()
+    test_render_mesh_triangle_soup()
     test_render_mesh_moderngl()
--- tests/test_triangle_soup_fitting.py
+++ tests/test_triangle_soup_fitting.py
@@ -1,16 +1,67 @@
 """Test using triangle soup rendering."""
 
 from deodr.examples.triangle_soup_fitting import run
+import json
 
 
-def test_soup():
-    final_loss = run(nb_max_iter=50, display=False, clockwise=False)
-    assert abs(final_loss[False] - 1331.141082325624) < 10
-    assert abs(final_loss[True] - 1457.4429407318612) < 20
+def test_soup(check_hashes=True):
 
-    final_loss = run(nb_max_iter=50, display=False, clockwise=True)
-    assert abs(final_loss[False] - 1331.141082325624) < 10
-    assert abs(final_loss[True] - 1457.4429407318612) < 20
+    losses, hashes = run(
+        nb_max_iter=50, display=False, clockwise=False, antialiase_error=False
+    )
+    assert abs(losses[-1] - 1331.141082325624) < 10
+
+    if check_hashes:
+        assert (
+            hashes[0]
+            == "38b6f6954374230aeb1ce5d804308522f6b4c58a6736a040aeef7f2176a20b28"
+        )
+        assert (
+            hashes[1]
+            == "0434ea722edb9e3364da9b0e8564c3002b9aa3b12791ba8f089689beecd3c4e9"
+        )
+
+    losses, hashes = run(
+        nb_max_iter=50, display=False, clockwise=False, antialiase_error=True
+    )
+    assert abs(losses[-1] - 1457.4429407318612) < 20
+    if check_hashes:
+        assert (
+            hashes[0]
+            == "82a7b73fde3615ef7c70008965f4bfda8610b9001c20dd435a880bf45a31d3d6"
+        )
+        assert (
+            hashes[1]
+            == "0de2e8b80730cfc444d0552cd81e5071897a525ec6495e643ca17fb0792496c0"
+        )
+
+    losses, hashes = run(
+        nb_max_iter=50, display=False, clockwise=True, antialiase_error=False
+    )
+    assert abs(losses[-1] - 1331.141082325624) < 10
+    if check_hashes:
+        assert (
+            hashes[0]
+            == "eb9f335acdeb2abc3e029826a56fd77bd7a4bb39c2794d9972735a9f388ddeba"
+        )
+        assert (
+            hashes[1]
+            == "6b4cc11e56dfebe2b8235485ea1bb8230a5765e7f0eeb2ac671d9cd4e3311f74"
+        )
+
+    losses, hashes = run(
+        nb_max_iter=50, display=False, clockwise=True, antialiase_error=True
+    )
+    assert abs(losses[-1] - 1457.4429407318612) < 20
+    if check_hashes:
+        assert (
+            hashes[0]
+            == "d2113e271f29afbdac393767297b923caaa4fe19af4bdd16d3b649c2f5ba3103"
+        )
+        assert (
+            hashes[1]
+            == "8820a5f90b4c0496b88542f42ed773afcd7cc0e0e3edc7a3da6427e2ab881d44"
+        )
 
 
 if __name__ == "__main__":
