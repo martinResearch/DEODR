4ad7fb6da46964fe0b5d4f2f9c08b16964cadfde
martin de la gorce <madelago@microsoft.com>
2023-07-14 00:59:44 +0100

    Update __init__.py

--- deodr/pytorch/__init__.py
+++ deodr/pytorch/__init__.py
@@ -11,8 +11,8 @@ __all__ = [
 
 
 from .differentiable_renderer_pytorch import CameraPytorch  # type: ignore
-from .differentiable_renderer_pytorch import Scene3DPytorch
+from .differentiable_renderer_pytorch import Scene3DPytorch # type: ignore
 from .laplacian_rigid_energy_pytorch import LaplacianRigidEnergyPytorch  # type: ignore
 from .mesh_fitter_pytorch import MeshDepthFitter  # type: ignore
-from .mesh_fitter_pytorch import MeshRGBFitterWithPose
+from .mesh_fitter_pytorch import MeshRGBFitterWithPose # type: ignore
 from .triangulated_mesh_pytorch import ColoredTriMeshPytorch  # type: ignore
